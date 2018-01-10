a4a5d589929590b8481eb2414be04abaacfcc134
Patrick <patrick.x.rivera@gmail.com>
2018-01-09 20:38:38 -0500

    Write get voices functionality

--- scripts.js
+++ scripts.js
@@ -4,3 +4,17 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = getVoices(this);
+  console.log(voices);
+}
+
+function getVoices(speechObj) {
+  let voicesArray = speechObj.getVoices();
+  let voices = voicesArray.map(voice => voice.name);
+  return voices;
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
